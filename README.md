1482f051a9d9cfd2ce68ffafba058edd71180b2a
Hidayat Abisena <hidayatabisena@gmail.com>
2021-02-20 15:24:51 +0700

    Final code

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Getting Started with Create React App
+# Form Kontak terintegrasi dengan GoogleSheet
+
+![Image preview]()
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState('');
+	const [email, setAge] = useState('');
+	const [city, setSalary] = useState('');
+	const [hobby, setHobby] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const objt = { name, email, city, hobby };
+
+		axios
+			.post(
+				'https://sheet.best/api/sheets/290f8b68-8813-4ad9-8925-96a5659d2775',
+				objt
+			)
+			.then((response) => {
+				console.log(response);
+			});
+	};
+
+	return (
+		<Container fluid className="container">
+			<Header as="h2">Form Kontak Googlesheet</Header>
+			<Form className="form">
+				<Form.Field>
+					<label>Name</label>
+					<input
+						placeholder="Enter your Name"
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>Email</label>
+					<input
+						placeholder="Enter your email"
+						onChange={(e) => setAge(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>City</label>
+					<input
+						placeholder="Enter your city"
+						onChange={(e) => setSalary(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>Hobby</label>
+					<input
+						placeholder="Enter your hobby"
+						onChange={(e) => setHobby(e.target.value)}
+					/>
+				</Form.Field>
+
+				<Button color="instagram" type="submit" onClick={handleSubmit}>
+					Submit
+				</Button>
+			</Form>
+		</Container>
+	);
 }
 
 export default App;
+
